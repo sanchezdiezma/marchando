cf2120de59466f5f6dad59f84d5bbad6d663478e
AlexVonrez <alexvonrez1@gmail.com>
2021-12-10 10:32:11 +0100

    Viernes

--- client/src/components/pages/RestaurantList/RestaurantCard.js
+++ client/src/components/pages/RestaurantList/RestaurantCard.js
@@ -1,29 +1,68 @@
-import React from 'react'
-import { Card, Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import './RestaurantsPage.css'
+import React, { Component } from "react";
+import { Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./RestaurantsPage.css";
+import AdminService from "../../../services/admin.service";
 
-const RestaurantCard = ({ name, direction, description, priceRange, capacity, imageURL, typeOfKitchen, specialInfo,}) => {
-  return (
-    <Card className="coaster-card" style={{ width: '16rem' }}>
-      <Card.Img variant="top" src={imageURL} />
-      <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Text>
-          {direction}.
-          {description}.
-          {priceRange}.
-          {capacity}.
-          {typeOfKitchen}.
-          {specialInfo}.
-        </Card.Text>
+class RestaurantCard extends Component {
+  constructor(props) {
+    super();
 
-        <Link to={"/"}>
-          <Button variant="primary">Reserva</Button>
-        </Link>
-      </Card.Body>
-    </Card>
-  )
+    this.state = {
+      restaurant: props.restaurant,
+      status: props.restaurant.status,
+    };
+
+    this.adminService = new AdminService();
+  }
+
+  handleClick = (e, status) => {
+    console.log(this.state.restaurant._id);
+    this.adminService
+      .changeRestaurantStatus(this.state.restaurant._id, { status })
+      .then((response) => {
+        //this.setState({ status: response.data.status });
+        this.props.updatePendingRestaurants(this.adminService);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <Card className="coaster-card" style={{ width: "16rem" }}>
+        <Card.Img variant="top" src={this.props.restaurant.imageURL} />
+        <Card.Body>
+          <Card.Title>{this.props.restaurant.name}</Card.Title>
+
+          <Card.Text>
+            {this.props.restaurant.direction}
+            <hr></hr>
+            {this.props.restaurant.description}
+            <hr></hr>
+            {this.props.restaurant.priceRange}
+            <hr></hr>
+            {this.props.restaurant.capacity}
+            <hr></hr>
+            {this.props.restaurant.typeOfKitchen}
+            <hr></hr>
+            {this.props.restaurant.specialInfo}
+            <hr></hr>
+            {this.props.restaurant.status}
+          </Card.Text>
+
+          <Link to={"/"}>
+            <Button variant="primary">Reserva</Button>
+          </Link>
+          <Button onClick={(e) => this.handleClick(e, "ACCEPTED")}>
+            Aceptar
+          </Button>
+          <Button onClick={(e) => this.handleClick(e, "REJECTED")}>
+            Rechazar
+          </Button>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
-export default RestaurantCard
+export default RestaurantCard;
--- client/src/services/admin.service.js
+++ client/src/services/admin.service.js
@@ -1,17 +1,19 @@
-import axios from 'axios'
+import axios from "axios";
 
 class AdminService {
   constructor() {
     this.app = axios.create({
-      baseURL: 'http://localhost:5005/admin',
-      withCredentials: true
-    })
+      baseURL: "http://localhost:5005/admin",
+      withCredentials: true,
+    });
   }
 
-  getPendingRestaurants = () => this.app.get("/pendingRestaurants")
-  getOneRestaurant = (id) => this.app.get(`/${id}`)
-  createRestaurant = (restaurantData) => this.app.post("/newRestaurant", restaurantData)
-  updateOneRestaurant = () => this.app.get ("/pendingRestaurants")
+  getPendingRestaurants = () => this.app.get("/pendingRestaurants");
+  getOneRestaurant = (id) => this.app.get(`/${id}`);
+  createRestaurant = (restaurantData) =>
+    this.app.post("/newRestaurant", restaurantData);
+  changeRestaurantStatus = (id, status) =>
+    this.app.put(`/editRestaurant/${id}`, status);
 }
 
-export default AdminService
+export default AdminService;
