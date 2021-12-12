8e1ccc00eca9ced5a707cc539f1200bff56ee0ec
jmsd <sanchezdiezma@gmail.com>
2021-12-12 18:17:04 +0100

    Domingo

--- client/src/components/pages/Index/Home.js
+++ client/src/components/pages/Index/Home.js
@@ -1,12 +1,51 @@
 import "../Index/Home.css";
+import ImgSection1 from '../../../assets/2-5-pie.png'
+
 
 function Home() {
     return (
         <div>
+            
             <section className="hero fl-container fl-centerh fl-centerv">
-                <h1>Marchando</h1>
-                
+                <div>
+                    <h1>Marchando</h1>
+                    <p>¿Sin reserva para esta noche?</p>
+                </div>
             </section>
+
+            <section className="hero3 fl-container fl-centerh fl-centerv">
+                <div>
+                    <img className="imgHero3" src={ImgSection1} alt="ImgSection1" />
+                </div>
+                
+                <div className="text"> 
+                    <p>¿Sin reserva para esta noche?</p>
+                    <p>¿Sin reserva para esta noche?</p>
+                    <p>¿Sin reserva para esta noche?</p>
+                </div>
+
+            </section>
+
+            
+
+                    
+                
+            
+           
+
+            <section className="hero4">
+
+            </section>
+
+            <section className="heroFooter">
+
+            </section>
+            
+
+
+                
+                
+                
 
             
                
