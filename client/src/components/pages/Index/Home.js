871afe5925acd4e47123d3157e37ad3cb936a572
jmsd <sanchezdiezma@gmail.com>
2021-12-12 10:38:11 +0100

    Css Home

--- client/src/components/pages/Index/Home.js
+++ client/src/components/pages/Index/Home.js
@@ -3,9 +3,13 @@ import "../Index/Home.css";
 function Home() {
     return (
         <div>
-            <section className="hero fl-container fl-centerv">
+            <section className="hero fl-container fl-centerh fl-centerv">
                 <h1>Marchando</h1>
+                
             </section>
+
+            
+               
 
 
            
