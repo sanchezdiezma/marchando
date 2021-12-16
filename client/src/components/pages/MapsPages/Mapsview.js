82923dc38649bb735f623a7aeba24b005f0b10c1
AlexVonrez <alexvonrez1@gmail.com>
2021-12-16 10:08:00 +0100

    Maps de Jesus

--- client/src/components/pages/MapsPages/Mapsview.js
+++ client/src/components/pages/MapsPages/Mapsview.js
@@ -1,34 +1,33 @@
-import React, { Component } from "react";
-// import { InputGroup, FormControl } from "react-bootstrap";
-import Maps from "../../Maps";
+// import React, { Component } from "react";
+// // import { InputGroup, FormControl } from "react-bootstrap";
 
-class Mapsview extends Component {
-  constructor(props) {
-    super(props);
+// class Mapsview extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      loggedUser: this.props.loggedUser,
-    };
-  }
-  render() {
-    return (
-      <div>
-        {/* <InputGroup className="mb-12">
-          <InputGroup.Text id="inputGroup-sizing-default">
-            Busca un restaurante
-          </InputGroup.Text> 
-        <FormControl
-          aria-label="Default"
-          aria-describedby="inputGroup-sizing-default"
-        />
-       </InputGroup>
-        <div className="container"> */}
-        <h1>Estos son los restaurantes</h1>
-        <Maps loggedUser={this.props.loggedUser} />
-      </div>
-      // </div>
-    );
-  }
-}
+//     this.state = {
+//       loggedUser: this.props.loggedUser,
+//     };
+//   }
+//   render() {
+//     return (
+//       <div>
+//         {/* <InputGroup className="mb-12">
+//           <InputGroup.Text id="inputGroup-sizing-default">
+//             Busca un restaurante
+//           </InputGroup.Text>
+//         <FormControl
+//           aria-label="Default"
+//           aria-describedby="inputGroup-sizing-default"
+//         />
+//        </InputGroup>
+//         <div className="container"> */}
+//         <h1>Estos son los restaurantes</h1>
+//         <Maps loggedUser={this.props.loggedUser} />
+//       </div>
+//       // </div>
+//     );
+//   }
+// }
 
-export default Mapsview;
+// export default Mapsview;
